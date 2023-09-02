98c151fdbb589ed17336fd85d9b46e6967e79c4f
Taranchuk <nfhfyxer@gmail.com>
2023-09-02 13:05:37 +0300

    Initial project

---
+++ 1.4/Source/VFETribalsMod.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace VFETribals
+{
+    public class VFETribalsMod : Mod
+    {
+        public VFETribalsMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("VFETribalsMod").PatchAll();
+        }
+    }
+}
