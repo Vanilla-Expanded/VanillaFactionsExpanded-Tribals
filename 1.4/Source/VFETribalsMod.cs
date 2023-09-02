1ea7e28dcbddf2f9b50ab6e0adfd613d2dba3546
Taranchuk <nfhfyxer@gmail.com>
2023-09-02 13:31:54 +0300

    add scenario and faction placeholders

--- 1.4/Source/VFETribalsMod.cs
+++ 1.4/Source/VFETribalsMod.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Verse;
@@ -11,6 +13,92 @@ namespace VFETribals
         public VFETribalsMod(ModContentPack pack) : base(pack)
         {
 			new Harmony("VFETribalsMod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(CompGatherSpot), "CompGetGizmosExtra")]
+    public static class CompGatherSpot_CompGetGizmosExtra_Patch
+    {
+        public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> __result)
+        {
+            foreach (var g in __result)
+            {
+                yield return g;
+            }
+
+            yield return new Command_Action
+            {
+                defaultLabel = "DEV: customize cornerstones",
+                action = () =>
+                {
+                    Find.WindowStack.Add(new Window_CustomizeCornerStones());
+                }
+            };
+            yield return new Command_Action
+            {
+                defaultLabel = "DEV: increase cornerstone point",
+                action = () =>
+                {
+                    GameComponent_Tribals.Instance.IncrementCornerstonePoint();
+                }
+            };
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class HotSwappableAttribute : Attribute
+    {
+    }
+
+    [HotSwappable]
+    public class Window_CustomizeCornerStones : Window
+    {
+        public override void DoWindowContents(Rect inRect)
+        {
+        }
+    }
+
+    [HotSwappable]
+    public class GameComponent_Tribals : GameComponent
+    {
+        public int availableCornerStonePoints;
+
+        public TechLevel playerTechLevel;
+
+        public static GameComponent_Tribals Instance;
+
+        public GameComponent_Tribals()
+        {
+            Instance = this;
+        }
+
+        public GameComponent_Tribals(Game game)
+        {
+            Instance = this;
+        }
+
+        public void IncrementCornerstonePoint()
+        {
+            availableCornerStonePoints++;
+        }
+
+        public override void FinalizeInit()
+        {
+            base.FinalizeInit();
+            Log.Message("FinalizeInit: " + Faction.OfPlayer.def.techLevel);
+        }
+
+        public override void StartedNewGame()
+        {
+            base.StartedNewGame();
+            Log.Message("StartedNewGame: " + Faction.OfPlayer.def.techLevel);
+            playerTechLevel = Faction.OfPlayer.def.techLevel;
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref availableCornerStonePoints, "availableCornerStonePoints");
         }
     }
 }
