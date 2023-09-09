e154cf96038c39edddc3e79b8610f4222973e4cd
Taranchuk <nfhfyxer@gmail.com>
2023-09-09 16:39:23 +0300

    wild men quest

---
+++ 1.4/Source/HarmonyPatches/StorytellerComp_IncidentChanceFinal_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace VFETribals
+{
+    [HarmonyPatch(typeof(StorytellerComp), "IncidentChanceFinal")]
+    public static class StorytellerComp_IncidentChanceFinal_Patch
+    {
+        public static void Postfix(IncidentDef def, ref float __result)
+        {
+            if (typeof(IncidentWorker_RaidEnemy).IsAssignableFrom(def.workerClass) 
+                && Faction.OfPlayer.def.techLevel == TechLevel.Animal)
+            {
+                __result *= 0.25f;
+            }
+        }
+    }
+}
--- 1.4/Source/Utils.cs
+++ 1.4/Source/Utils.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RimWorld;
+using RimWorld.Planet;
+using RimWorld.QuestGen;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Verse;
@@ -53,6 +56,88 @@ namespace VFETribals
             }
             research = null;
             return true;
+        }
+
+        public static QuestPart_WildmenPostExpire WildmenPostExpire(MapParent mapParent, List<Pawn> wildmen, string inSignal = null, 
+            QuestPart.SignalListenMode signalListenMode = QuestPart.SignalListenMode.OngoingOnly, Faction faction = null)
+        {
+            QuestPart_WildmenPostExpire postexpire = new QuestPart_WildmenPostExpire();
+            postexpire.inSignal = QuestGenUtility.HardcodedSignalWithQuestID(inSignal) ?? QuestGen.slate.Get<string>("inSignal");
+            postexpire.signalListenMode = signalListenMode;
+            postexpire.mapParent = mapParent;
+            postexpire.Pawns = wildmen;
+            postexpire.faction = faction;
+            QuestGen.quest.AddPart(postexpire);
+            return postexpire;
+        }
+
+
+        public static void MakeWildmenRaid(List<Pawn> wildmen, Map map, TaggedString letterLabel = default, 
+            TaggedString letterText = default)
+        {
+            var dict = new Dictionary<Pawn, int>();
+            foreach (var w in wildmen)
+            {
+                dict.Add(w, 0);
+            }
+            IncidentParms parms = new IncidentParms
+            {
+                target = map,
+                points = wildmen.Sum(x => x.kindDef.combatPower),
+                faction = wildmen.First().Faction,
+                pawnGroups = dict,
+            };
+            var worker = IncidentDefOf.RaidEnemy.Worker as IncidentWorker_RaidEnemy;
+            TryGenerateRaidInfo(worker, parms, wildmen);
+            if (letterLabel == default)
+            {
+                letterLabel = worker.GetLetterLabel(parms);
+            }
+            if (letterText == default)
+            {
+                letterText = worker.GetLetterText(parms, wildmen);
+            }
+            PawnRelationUtility.Notify_PawnsSeenByPlayer_Letter(wildmen, ref letterLabel, ref letterText,
+                worker.GetRelatedPawnsInfoLetterText(parms), informEvenIfSeenBefore: true);
+            List<TargetInfo> list = new List<TargetInfo>();
+            if (parms.pawnGroups != null)
+            {
+                List<List<Pawn>> list2 = IncidentParmsUtility.SplitIntoGroups(wildmen, parms.pawnGroups);
+                List<Pawn> list3 = list2.MaxBy((List<Pawn> x) => x.Count);
+                if (list3.Any())
+                {
+                    list.Add(list3[0]);
+                }
+                for (int i = 0; i < list2.Count; i++)
+                {
+                    if (list2[i] != list3 && list2[i].Any())
+                    {
+                        list.Add(list2[i][0]);
+                    }
+                }
+            }
+            else if (wildmen.Any())
+            {
+                foreach (Pawn item in wildmen)
+                {
+                    list.Add(item);
+                }
+            }
+            worker.SendStandardLetter(letterLabel, letterText, worker.GetLetterDef(), parms, list);
+            if (parms.controllerPawn == null || parms.controllerPawn.Faction != Faction.OfPlayer)
+            {
+                parms.raidStrategy.Worker.MakeLords(parms, wildmen);
+            }
+        }
+
+        public static void TryGenerateRaidInfo(IncidentWorker_RaidEnemy worker, IncidentParms parms, List<Pawn> pawns)
+        {
+            parms.raidStrategy = RaidStrategyDefOf.ImmediateAttack;
+            parms.raidArrivalMode = PawnsArrivalModeDefOf.EdgeWalkIn;
+            worker.ResolveRaidAgeRestriction(parms);
+            parms.raidArrivalMode.Worker.TryResolveRaidSpawnCenter(parms);
+            worker.GenerateRaidLoot(parms, parms.points, pawns);
+            parms.raidArrivalMode.Worker.Arrive(pawns, parms);
         }
     }
 }
