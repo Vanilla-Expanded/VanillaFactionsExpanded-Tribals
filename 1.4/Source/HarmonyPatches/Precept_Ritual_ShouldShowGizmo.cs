1bebccdba34d22a25f4b2cb3a8c37a308aab36e0
SargBjornson <sarg.auxiliar@gmail.com>
2023-10-16 15:27:03 +0200

    Hiding tribal gathering icon beyond Neolithic tech

---
+++ 1.4/Source/HarmonyPatches/Precept_Ritual_ShouldShowGizmo.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace VFETribals
+{
+    [HarmonyPatch(typeof(Precept_Ritual), "ShouldShowGizmo")]
+    public static class VFETribals_Precept_Ritual_ShouldShowGizmo_Patch
+    {
+        public static void Postfix(ref bool __result, Precept_Ritual __instance)
+        {
+           if(__instance.def == VFET_DefOf.VFET_TribalGathering)
+            {
+                if (Faction.OfPlayer.def.techLevel > TechLevel.Neolithic)
+                {
+                    __result = false;
+                }
+
+            }
+        }
+    }
+}
