0107e8a85dfcb120467d98483fa861c58d7446d3
Taranchuk <nfhfyxer@gmail.com>
2024-08-13 19:31:00 +0300

    tribals tech level fix + removed research spot requirement from complex furnitures

--- 1.5/Source/Startup.cs
+++ 1.5/Source/Startup.cs
@@ -1,14 +1,34 @@
 ﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
 using Verse;
+using Verse.Profile;
 
 namespace VFETribals
 {
     [StaticConstructorOnStartup]
     public static class Startup
     {
+        public static Dictionary<FactionDef, TechLevel> originalTechLevels = new Dictionary<FactionDef, TechLevel>();
         static Startup()
         {
             new Harmony("VFETribalsMod").PatchAll();
+            foreach (var faction in DefDatabase<FactionDef>.AllDefs)
+            {
+                originalTechLevels[faction] = faction.techLevel;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MemoryUtility), "UnloadUnusedUnityAssets")]
+    public static class MemoryUtility_UnloadUnusedUnityAssets_Patch
+    {
+        public static void Postfix()
+        {
+            foreach (var kvp in Startup.originalTechLevels)
+            {
+                kvp.Key.techLevel = kvp.Value;
+            }
         }
     }
 }
